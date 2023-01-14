82fc3eaccef35d3af70c5fd092a3ccb279ae9d97
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-01-13 22:35:15 -0300

    chore: testing gh actions

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>GitHub Actions | CI-Flow</h1>
+      <h1>GitHub Actions | CI-Flow!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
